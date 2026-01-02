36a4bbc39bbcc90272b9dbc1c9475ff50f20fb66
Daksh Singh <daksh.singh.2407@gmail.com>
2026-01-02 12:18:14 +0530

    Initial commit

---
+++ interview.js
@@ -0,0 +1,20 @@
+
+// const str = "Daksh";
+// const str1 = 18;
+//
+// function merge(string1, string2) {
+//     const str_n1 = string1.toString();
+//     const str_n2 = string2.toString();
+//     let str_res = "";
+//     for (let i = 0; i < Math.max(str_n1.length, str_n2.length); i++) {
+//         str_res += (str_n1[i] || "") + (str_n2[i] || "");
+//     }
+//     return str_res;
+// }
+//
+// const str_res = merge(str, str1);
+// console.log(str_res);
+
+
+function abc(...abc) { console.log(abc, typeof abc) }
+abc("Daksh");
---
+++ main.js
@@ -0,0 +1,57 @@
+
+function sumArray(array) { return array.reduce((prev, curr) => curr + prev) }
+function isPalindrome(string) { return string.toLowerCase() === string.toLowerCase().split("").reverse().join("") }
+function converts(array) { return array.join(" ") }
+
+function maxDifferenceBwTwoElementsOfArray(array) {
+    let max = 0;
+    for ( let i = 0; i < array.length; i++ ) {
+        for ( let j = i + 1; j < array.length; j++ ) {
+            if ( Math.abs(array[j] - array[i]) > max ) { max = Math.abs(array[j] - array[i]) }
+        }
+    }
+    return max;
+}
+
+function isPrime(number) {
+    for ( let i = 2; i < number/2; i++ ) {
+        if ( number % 2 === 0 ) { return false }
+    }
+    return true;
+}
+
+function recursiveFactorial(number) {
+    if ( number === 0 ) { return 1 }
+    return number * recursiveFactorial(number - 1);
+}
+
+function removeDuplicates(array) {
+    let outArr = [];
+    array.forEach((ele) => { if (!outArr.includes(ele)) {outArr.push(ele)} });
+    return outArr;
+}
+
+function removeDuplicatesIrrSign(array) {
+    let outArr = [];
+    array.forEach((ele) => { if (!outArr.includes(Math.abs(ele))) {outArr.push(ele)} });
+    return outArr;
+}
+
+function mergeArray(arr1, arr2) {
+    let outArr = [];
+    arr1.forEach((ele) => outArr.push(ele));
+    arr2.forEach((ele) => { if (!outArr.includes(ele)) {outArr.push(ele)} });
+    return outArr;
+}
+
+
+console.log(sumArray([1, 2, 3, 4, 5]));
+console.log(isPalindrome("Huhu"))
+console.log(maxDifferenceBwTwoElementsOfArray([1, 2, 3, 4, -9]));
+console.log(isPrime(11));
+console.log(recursiveFactorial(6));
+console.log(removeDuplicates([1, 2, 3, 4, 9, 1]));
+console.log(mergeArray([1, 2, 3, 4, 9, 1], [1, 2, 3, 4, -9]));
+console.log(removeDuplicates(mergeArray([1, 2, 3, 4, 9, 1], [1, 2, 3, 4, -9])));
+console.log(removeDuplicatesIrrSign(mergeArray([1, 2, 3, 4, 9, 1], [1, 2, 3, 4, -9])));
+console.log(converts(["Daksh", "Singh", "is", "a", "good", "boy"]));
