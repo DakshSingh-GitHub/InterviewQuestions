26c17293ebdf94d81b31fb006302b83cd11716a2
Daksh Singh <daksh.singh.2407@gmail.com>
2026-01-05 19:20:17 +0530

    Dated: 5th January 2026

--- question_daily.js
+++ question_daily.js
@@ -1,10 +1,18 @@
 
-function fnc(x, y) {
-    console.log(arguments)
-    arguments[1] = 15;
-    console.log(arguments);
-    return x + y;
+
+function countVowels(string) {
+    const vowels = ["a", "e", "i", "o", "u"];
+    let count = 0;
+    string
+        .toLowerCase()
+        .split("")
+        .forEach((ele) => {if (vowels.includes(ele)) { count += 1 }});
+    return count;
 }
+console.log(countVowels("ABCDE"));
 
-console.log(fnc(5, 5));
+const arr = [1, 2, 3, 4, 5]
+const k = 2;
 
+const result = arr.slice(k).concat(arr.splice(0, k));
+console.log(result);
