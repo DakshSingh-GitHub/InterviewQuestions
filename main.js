b37510d1fda03199b6d9ce3b0adf5b319356c702
Daksh Singh <daksh.singh.2407@gmail.com>
2026-01-12 14:03:18 +0530

    Dated: 12th January commit

--- main.js
+++ main.js
@@ -1,9 +1,10 @@
 
-function sumArray(array) { return array.reduce((prev, curr) => curr + prev) }
-function isPalindrome(string) { return string.toLowerCase() === string.toLowerCase().split("").reverse().join("") }
-function converts(array) { return array.join(" ") }
+export function sumArray(array) { return array.reduce((prev, curr) => curr + prev) }
+export function isPalindrome(string) { return string.toLowerCase() === string.toLowerCase().split("").reverse().join("") }
+export function converts(array) { return array.join(" ") }
+export function findSubstring(arr) { return arr[0].includes(arr[1]) }
 
-function maxDifferenceBwTwoElementsOfArray(array) {
+export function maxDifferenceBwTwoElementsOfArray(array) {
     let max = 0;
     for ( let i = 0; i < array.length; i++ ) {
         for ( let j = i + 1; j < array.length; j++ ) {
@@ -13,50 +14,63 @@ function maxDifferenceBwTwoElementsOfArray(array) {
     return max;
 }
 
-function isPrime(number) {
+export function isPrime(number) {
     for ( let i = 2; i < number/2; i++ ) {
         if ( number % 2 === 0 ) { return false }
     }
     return true;
 }
 
-function recursiveFactorial(number) {
+export function recursiveFactorial(number) {
     if ( number === 0 ) { return 1 }
     return number * recursiveFactorial(number - 1);
 }
 
-function removeDuplicates(array) {
+export function removeDuplicates(array) {
     let outArr = [];
     array.forEach((ele) => { if (!outArr.includes(ele)) {outArr.push(ele)} });
     return outArr;
 }
 
-function removeDuplicatesIrrSign(array) {
+export function removeDuplicatesIrrSign(array) {
     let outArr = [];
     array.forEach((ele) => { if (!outArr.includes(Math.abs(ele))) {outArr.push(ele)} });
     return outArr;
 }
 
-function mergeArray(arr1, arr2) {
+export function mergeArray(arr1, arr2) {
     let outArr = [];
     arr1.forEach((ele) => outArr.push(ele));
     arr2.forEach((ele) => { if (!outArr.includes(ele)) {outArr.push(ele)} });
     return outArr;
 }
 
-function destructureMergeArray(arr1, arr2) {
+export function destructureMergeArray(arr1, arr2) {
     return removeDuplicates([...arr1, ...arr2]);
 }
 
+export function fibonacci(number) {
+    let num = [];
+    num[0] = 0; num[1] = 1;
+    for (let i = 2; i < number+1; i++) {
+        num[i] = num[i-1] + num[i-2];
+    }
+    return num;
+}
+export function sumOfFibonacciNumbers(number) { return fibonacci(number).reduce((prev, curr) => prev + curr) }
 
-console.log(sumArray([1, 2, 3, 4, 5]));
-console.log(isPalindrome("Huhu"))
-console.log(maxDifferenceBwTwoElementsOfArray([1, 2, 3, 4, -9]));
-console.log(isPrime(11));
-console.log(recursiveFactorial(6));
-console.log(removeDuplicates([1, 2, 3, 4, 9, 1]));
-console.log(mergeArray([1, 2, 3, 4, 9, 1], [1, 2, 3, 4, -9]));
-console.log(removeDuplicates(mergeArray([1, 2, 3, 4, 9, 1], [1, 2, 3, 4, -9])));
-console.log(removeDuplicatesIrrSign(mergeArray([1, 2, 3, 4, 9, 1], [1, 2, 3, 4, -9])));
-console.log(converts(["Daksh", "Singh", "is", "a", "good", "boy"]));
-console.log(destructureMergeArray([1, 2, 3, 4, 9, 1], [1, 2, 3, 4, -9]));
+
+// console.log(sumArray([1, 2, 3, 4, 5]));
+// console.log(isPalindrome("Huhu"))
+// console.log(maxDifferenceBwTwoElementsOfArray([1, 2, 3, 4, -9]));
+// console.log(isPrime(11));
+// console.log(recursiveFactorial(6));
+// console.log(removeDuplicates([1, 2, 3, 4, 9, 1]));
+// console.log(mergeArray([1, 2, 3, 4, 9, 1], [1, 2, 3, 4, -9]));
+// console.log(removeDuplicates(mergeArray([1, 2, 3, 4, 9, 1], [1, 2, 3, 4, -9])));
+// console.log(removeDuplicatesIrrSign(mergeArray([1, 2, 3, 4, 9, 1], [1, 2, 3, 4, -9])));
+// console.log(converts(["Daksh", "Singh", "is", "a", "good", "boy"]));
+// console.log(destructureMergeArray([1, 2, 3, 4, 9, 1], [1, 2, 3, 4, -9]));
+// console.log(findSubstring(["Daksh", "sh"]));
+// console.log(fibonacci(10));
+// console.log(sumOfFibonacciNumbers(10));
---
+++ questions_daily.js
@@ -0,0 +1,9 @@
+
+import { fibonacci } from "./main.js";
+
+
+let arr = ["chandan", "and"];
+function findSubstring(arr) { return arr[0].includes(arr[1]) }
+console.log(findSubstring(arr));
+
+console.log(fibonacci(4));
